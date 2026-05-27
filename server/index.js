cc3e3c728f3538ce64ec59daf2ea60f6a465ff50
maxson <adstik56@gmail.com>
2026-05-27 04:05:08 +0100

    Update CORS to allow Vercel domains

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ mongoose.connect(MONGODB_URI)
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', /.+\.vercel\.app$/],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin']
