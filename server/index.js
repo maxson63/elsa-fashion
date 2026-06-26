7cb64fe4cda888c3064bff96c992ca2dcdb1ea00
maxson <adstik56@gmail.com>
2026-06-26 12:06:57 +0100

    Add Netlify to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ mongoose.connect(MONGODB_URI, {
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', /.+\.vercel\.app$/],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', /.+\.vercel\.app$/, /.+\.netlify\.app$/],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin']
